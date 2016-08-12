65646120c74c6c07c873befbf78bc7f9ccab50d7
Betty van Aken <bettyvanaken@gmail.com>
2016-08-12 10:42:53 +0200

    add demo and calibration

--- public/listeners.js
+++
@@ -1,6 +0,0 @@
-
-function onLightChange(value, shaderPass) {
-
-	//copy gui params into shader uniforms
-	shaderPass.uniforms[ "amount" ].value = testParams.amount;
-}
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var restify = require('restify'),
     socketio = require('socket.io'),
     server = restify.createServer(),
     io = socketio.listen(server.server),
-	  uuid = require('node-uuid');
+    uuid = require('node-uuid');
 
 // for retrieving full responses
 server.use(restify.fullResponse())
@@ -13,21 +13,20 @@ routeSetup();
 sensorSetup();
 
 // Listening to the port
-server.listen(8080, function() {
-  console.log('%s listening at %s', server.name, server.url);
-  console.log("########################");
+server.listen(8080, function () {
+    console.log('%s listening at %s', server.name, server.url);
 });
 
 io.on('connection', function (socket) {
 
-  // React to client connection/disconnection
-  socket.on('registered', function (data) {
-    console.log("Client registered."); 
+    // React to client connection/disconnection
+    socket.on('registered', function (data) {
+        console.log("Client registered.");
 
-    socket.on('disconnect', function (payload) {
-        console.log("Client disconnected."); 
+        socket.on('disconnect', function (payload) {
+            console.log("Client disconnected.");
+        });
     });
-  });
 });
 
 // Setup of temperature, light and humidity sensor
@@ -35,146 +34,155 @@ io.on('connection', function (socket) {
 // A0: Humidity
 // A2: Temperature
 // A4: Light
-function sensorSetup(){
+function sensorSetup() {
 
-  var five = require('johnny-five');
-  var board = new five.Board();
+    var five = require('johnny-five');
+    var board = new five.Board();
 
-  board.on("ready", function() {
+    // Humidity changes very frequently, so a threshold for a change emit is set
+    var previousHumidity = 0;
+    var humChangeThreshold = 50;
 
-    console.log("Board ready.");
+    board.on("ready", function () {
 
-    // Humidity
-    var humidity = new five.Sensor({
-      pin: "A0",
-      freq: 250
+        console.log("Board ready.");
+
+        // Humidity
+        var humidity = new five.Sensor({
+            pin: "A0",
+            freq: 250
+        });
+
+        humidity.on("change", function (value) {
+            console.log("Humid: " + value);
+
+            if (Math.abs(previousHumidity - value) > humChangeThreshold) {
+                io.emit('updateHumid', value)
+            }
+        });
+
+        // Temperature
+        var temp = new five.Temperature({
+            pin: "A2",
+            controller: "TMP36",
+            freq: 250
+        });
+
+        temp.on("change", function () {
+            console.log("Temp: " + this.celsius);
+
+            io.emit('updateTemp', this.celsius)
+        });
+
+        // Light
+        var light = new five.Sensor({
+            pin: "A4",
+            freq: 250
+        });
+
+        light.on("change", function (value) {
+            console.log("Light: " + value);
+
+            io.emit('updateLight', value)
+        });
     });
-
-    humidity.on("change", function(value) {
-      console.log("Humid: " + value);
-
-      io.emit('updateHumid', value)
-    });
-
-    // Temperature
-    var temp = new five.Temperature({
-      pin: "A2",
-      controller: "TMP36",
-      freq: 250
-    });
-
-    temp.on("change", function() {
-      console.log("Temp: " + this.celsius);
-
-      io.emit('updateTemp', this.celsius)
-    });
-
-    // Light
-    var light = new five.Sensor({
-      pin: "A4",
-      freq: 250
-    });
-
-    light.on("change", function(value) {
-      console.log("Light: " + value);
-
-      io.emit('updateLight', value)
-    });
-  });
 }
 
 // Setup of Restify routes
-function routeSetup(){
+function routeSetup() {
 
-server.get('/live', restify.serveStatic({
-  directory: './public/',
-  file: 'room.html'
-}));
+    server.get('/start', restify.serveStatic({
+        directory: './public/',
+        file: 'room.html'
+    }));
 
-server.get('/stats', restify.serveStatic({
-  directory: './public/',
-  file: 'stats.html'
-}));
+    server.get('/helper/Detector.js', restify.serveStatic({
+        directory: './helper/',
+        file: 'Detector.js'
+    }));
 
-server.get('/helper/Detector.js', restify.serveStatic({
-  directory: './helper/',
-  file: 'Detector.js'
-}));
+    server.get('/helper/EffectComposer.js', restify.serveStatic({
+        directory: './helper/',
+        file: 'EffectComposer.js'
+    }));
 
-server.get('/helper/EffectComposer.js', restify.serveStatic({
-  directory: './helper/',
-  file: 'EffectComposer.js'
-}));
+    server.get('/helper/RenderPass.js', restify.serveStatic({
+        directory: './helper/',
+        file: 'RenderPass.js'
+    }));
 
-server.get('/helper/RenderPass.js', restify.serveStatic({
-  directory: './helper/',
-  file: 'RenderPass.js'
-}));
+    server.get('/helper/ShaderPass.js', restify.serveStatic({
+        directory: './helper/',
+        file: 'ShaderPass.js'
+    }));
 
-server.get('/helper/ShaderPass.js', restify.serveStatic({
-  directory: './helper/',
-  file: 'ShaderPass.js'
-}));
+    server.get('/helper/Fire.js', restify.serveStatic({
+        directory: './helper/',
+        file: 'Fire.js'
+    }));
 
-server.get('/helper/Fire.js', restify.serveStatic({
-  directory: './helper/',
-  file: 'Fire.js'
-}));
+    server.get('/helper/FireShader.js', restify.serveStatic({
+        directory: './helper/',
+        file: 'FireShader.js'
+    }));
 
-server.get('/helper/FireShader.js', restify.serveStatic({
-  directory: './helper/',
-  file: 'FireShader.js'
-}));
+    server.get('/libs/perlin.js', restify.serveStatic({
+        directory: './libs/',
+        file: 'perlin.js'
+    }));
 
-server.get('/libs/perlin.js', restify.serveStatic({
-  directory: './libs/',
-  file: 'perlin.js'
-}));
+    server.get('/libs/three.js', restify.serveStatic({
+        directory: './libs/',
+        file: 'three.js'
+    }));
 
-server.get('/libs/three.js', restify.serveStatic({
-  directory: './libs/',
-  file: 'three.js'
-}));
+    server.get('/libs/dat.gui.min.js', restify.serveStatic({
+        directory: './libs/',
+        file: 'dat.gui.min.js'
+    }));
 
-server.get('/TestShader.js', restify.serveStatic({
-  directory: './public/',
-  file: 'TestShader.js'
-}));
+    server.get('/LightTemperatureShader.js', restify.serveStatic({
+        directory: './public/',
+        file: 'LightTemperatureShader.js'
+    }));
 
-server.get('/textures/px.jpg', restify.serveStatic({
-  directory: './public/textures/',
-  file: 'px.jpg'
-}));
+    server.get('/textures/px.jpg', restify.serveStatic({
+        directory: './public/textures/',
+        file: 'px.jpg'
+    }));
 
-server.get('/textures/nx.jpg', restify.serveStatic({
-  directory: './public/textures/',
-  file: 'nx.jpg'
-}));
+    server.get('/textures/nx.jpg', restify.serveStatic({
+        directory: './public/textures/',
+        file: 'nx.jpg'
+    }));
 
-server.get('/textures/py.jpg', restify.serveStatic({
-  directory: './public/textures/',
-  file: 'py.jpg'
-}));
+    server.get('/textures/py.jpg', restify.serveStatic({
+        directory: './public/textures/',
+        file: 'py.jpg'
+    }));
 
-server.get('/textures/ny.jpg', restify.serveStatic({
-  directory: './public/textures/',
-  file: 'ny.jpg'
-}));
+    server.get('/textures/ny.jpg', restify.serveStatic({
+        directory: './public/textures/',
+        file: 'ny.jpg'
+    }));
 
-server.get('/textures/pz.jpg', restify.serveStatic({
-  directory: './public/textures/',
-  file: 'pz.jpg'
-}));
+    server.get('/textures/pz.jpg', restify.serveStatic({
+        directory: './public/textures/',
+        file: 'pz.jpg'
+    }));
 
-server.get('/textures/nz.jpg', restify.serveStatic({
-  directory: './public/textures/',
-  file: 'nz.jpg'
-}));
+    server.get('/textures/nz.jpg', restify.serveStatic({
+        directory: './public/textures/',
+        file: 'nz.jpg'
+    }));
 
-server.get('/textures/firetexture.png', restify.serveStatic({
-  directory: './public/textures/',
-  file: 'firetexture.png'
-}));
+    server.get('/textures/firetexture.png', restify.serveStatic({
+        directory: './public/textures/',
+        file: 'firetexture.png'
+    }));
 
+    server.get('/css/custom_style.css', restify.serveStatic({
+        directory: './public/css/',
+        file: 'custom_style.css'
+    }));
 }
-
